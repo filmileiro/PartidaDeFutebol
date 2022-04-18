3a7a11253e419dc1ac77185ef0e1aed89f69c5b4
William B. Pereira <filmileirodevop@gmail.com>
2022-04-18 10:22:30 -0300

    corrigindo erros.

---
+++ src/PartidaDeFutebol.java
@@ -0,0 +1,125 @@
+import java.util.Locale;
+import java.util.Random;
+import java.util.Scanner;
+
+public class PartidaDeFutebol {
+
+    private String LocalDoJogo;
+    private String NomeDoTimeMandante;
+    private String NomeDoTimeVisitante;
+    private Integer QuantidadeDeJogadoresA;
+    private Integer QuantidadeDeJogadoresB;
+    private Integer GolTimeA = 0;
+    private Integer GolTimeB = 0;
+
+    Random ApitoDoJuiz = new Random();
+    private Integer Acoes;
+
+    public void dadosDaPartida(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Informe o local da Partida: ");
+        this.LocalDoJogo = sc.nextLine();
+        System.out.print("Informe o nome do time Mandante: ");
+        this.NomeDoTimeMandante = sc.nextLine();
+        System.out.print("Informe o nome do time Visitante: ");
+        this.NomeDoTimeVisitante = sc.nextLine();
+        System.out.printf("Informe a quantidade de Jogadores do %s: ", NomeDoTimeMandante);
+        this.QuantidadeDeJogadoresA = sc.nextInt();
+        System.out.printf("Informe a quantidade de Jogadores do %s: ", NomeDoTimeVisitante);
+        this.QuantidadeDeJogadoresB = sc.nextInt();
+
+        sc.close();
+    }
+    public void verificaStatusDaPartida(){
+        if (QuantidadeDeJogadoresA <= 7){
+            System.out.printf("%nA regra é clara! Quando a quantidade de jogadores é 7 ou menos que 7 o " +
+                    "juiz não altoriza o inicio do Jogo. então... O %s perde o jogo.%n", this.NomeDoTimeMandante);
+        }else if(QuantidadeDeJogadoresB <= 7){
+            System.out.printf("%nA regra é clara! Quando a quantidade de jogadores é 7 ou menos que 7 o " +
+                    "juiz não altoriza o inicio do Jogo. então... O %s perde o jogo.%n", this.NomeDoTimeVisitante);
+        }else{
+            System.out.printf("%nTudo pronto para começar o Primeiro tempo%n");
+            System.out.println();
+            System.out.println("-------------//----------------");
+        }
+    }
+
+    public void iniarPrimeitoTempo(){
+        if ((QuantidadeDeJogadoresA > 7 && QuantidadeDeJogadoresA<=11) && (QuantidadeDeJogadoresB > 7 && QuantidadeDeJogadoresB <= 11)){
+            System.out.println();
+            System.out.println("APITA O ARBITRO!!!");
+            for (int i = 0; i < 45 ; i+=3) {
+
+                if (i + 1 == 1){
+                    System.out.println("COMEÇA O PRIMEIRO TEMPO!!");
+                }else{
+                    Acoes = ApitoDoJuiz.nextInt(10)+1;
+                    System.out.println();
+                    System.out.println( i + " MINUTOS DO PRIMEIRO TEMPO");
+                    switch (Acoes){
+                        case 1:
+                            System.out.printf("GOOOOOOL!! DO %s !!!!", NomeDoTimeMandante.toUpperCase(Locale.ROOT));
+                            System.out.println();
+                            GolTimeA++;
+                            break;
+                        case 2:
+                            System.out.printf("GOOOOOOL!! DO %s !!!!", NomeDoTimeVisitante.toUpperCase(Locale.ROOT));
+                            System.out.println();
+                            GolTimeB++;
+                            break;
+                        case 3:
+                            System.out.printf("FALTA DO %s !!!!", NomeDoTimeMandante.toUpperCase(Locale.ROOT));
+                            System.out.println();
+                            break;
+                        case 4:
+                            System.out.printf("FALTA DO %s !!!!", NomeDoTimeVisitante.toUpperCase(Locale.ROOT));
+                            System.out.println();
+                            break;
+                        case 5:
+                            System.out.printf("LATERAL PARA O %s !!!!", NomeDoTimeMandante.toUpperCase(Locale.ROOT));
+                            System.out.println();
+                            break;
+                        case 6:
+                            System.out.printf("LATERAL PARA O %s !!!!", NomeDoTimeVisitante.toUpperCase(Locale.ROOT));
+                            System.out.println();
+                            break;
+                        case 7:
+                            System.out.printf("IMPEDIMENTO PARA O %s !!!!", NomeDoTimeMandante.toUpperCase(Locale.ROOT));
+                            System.out.println();
+                            break;
+                        case 8:
+                            System.out.printf("IMPEDIMENTO PARA O %s !!!!", NomeDoTimeVisitante.toUpperCase(Locale.ROOT));
+                            System.out.println();
+                            break;
+                        case 9:
+                            System.out.printf("CHUTOU PRA FORA! INACREDITAVEL FUTEBOL CLUBE!!");
+                            System.out.println();
+                            break;
+
+                        default:
+                            System.out.println("JUIZ VAI TER QUE VER NO VAR!!");
+                            break;
+
+
+                    }
+                }
+
+            }
+            System.out.println();
+            System.out.println("FIM DO PRIMEIRO TEMPO!");
+            System.out.println();
+            System.out.println();
+            System.out.println("------------------------------------------");
+            System.out.printf("PLACAR ATUAL É: %s %d X %d %s%n", NomeDoTimeMandante.toUpperCase(Locale.ROOT), GolTimeA, GolTimeB, NomeDoTimeVisitante.toUpperCase(Locale.ROOT));
+            System.out.println("------------------------------------------");
+            System.out.println();
+            System.out.println();
+
+
+
+        }
+    }
+
+
+}
---
+++ src/Partidapricipal.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Partidapricipal {
+    public static void main(String[] args) {
+
+    PartidaDeFutebol  j1 = new PartidaDeFutebol();
+
+        j1.dadosDaPartida();
+        j1.verificaStatusDaPartida();
+        j1.iniarPrimeiroTempo();
+
+
+    }
+}
